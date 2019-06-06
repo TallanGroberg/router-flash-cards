f9b0161535c7054b2a18bcb13c810091d9027ac0
tallan groberg <tallan.taven@gmail.com>
2019-06-06 14:53:56 -0600

    initial routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Header, } from "semantic-ui-react";
+import { Route, Switch, } from 'react-router-dom';
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import NoMatch from './components/NoMatch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+
+      <Navbar />
+        <Container style={{ marginTop: "25px", }}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route component={NoMatch} />
+            </Switch>
+        </Container>
+      </>
+      )
+  }
 }
+
+
+
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Header, } from "semantic-ui-react";
+
+const About = () => (
+  <div><h2>About</h2></div>
+);
+
+export default About;
